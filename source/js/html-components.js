741fe41be7eacdf9a7b5c429be9569402c8ad1dd
Reutskov-Dmitriy <infowelc@gmail.com>
2022-10-26 16:16:14 +0300

    add header component

--- source/js/html-components.js
+++ source/js/html-components.js
@@ -1,3 +1,36 @@
+//HTML HEADER
+class ComponentHeader extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+		<header class="header">
+		<nav class="header-menu">
+			<div class="header-menu__logo logo">
+				<h1 class="logo__title logo__title--position logo__title--size"><a href="../../../index.html#home">Hello English</a>
+				</h1>
+			</div>
+			<input type="checkbox" id="checkbox" class="header-menu__checkbox" hidden>
+			<label for="checkbox" class="header-menu__btn">
+				<div class="header-menu__icon">
+					<span></span>
+				</div>
+			</label>
+			<div class="header-menu__container">
+				<ul class="header-menu__items">
+					<li class="header-menu__item"><a href="../../../index.html#home" class="header-menu__link"> Home</a></li>
+					<li class="header-menu__item"><a href="../../../index.html#section-levels" class="header-menu__link">Study With Us</a>
+					</li>
+					<li class="header-menu__item"><a href="../../../source/pages/contacts.html" class="header-menu__link">Contact
+							Us</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>`
+	}
+}
+
+customElements.define('component-header', ComponentHeader);
+
+//HTML FOOTER
 class ComponentFooter extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
