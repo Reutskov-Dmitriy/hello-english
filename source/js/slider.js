a5ba809ac387fa83706c5e83bb1a34d911942ddc
Reutskov-Dmitriy <infowelc@gmail.com>
2022-10-27 22:03:17 +0300

    change link html

--- source/js/slider.js
+++ source/js/slider.js
@@ -9,18 +9,4 @@ $(document).ready(function () {
 		pauseonHover: true,
 	});
 
-	$('.autoplay').slick({
-		arrows: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplay: true,
-		speed: 1000,
-		autoplaySpeed: 4000,
-		adaptiveHeight: true,
-		dots: true,
-		pauseOnHover: true,
-		pauseOnDotsHover: true,
-
-	})
-
 });
